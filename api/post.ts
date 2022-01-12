63b38f4f0e53ae5b40aff3d740966726b860a210
Mikael Porttila <mikael.porttila@gmail.com>
2022-01-12 22:48:09 +0100

    some ideas...

--- api/post.ts
+++ api/post.ts
@@ -1,6 +1,15 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
+/* import 'isomorphic-fetch'; */
 
-export default (request: VercelRequest, response: VercelResponse) => {
-  const { name } = request.query;
-  response.status(200).send(`Hello ${name}!!!`);
+export default async (request: VercelRequest, response: VercelResponse) => {
+    /* console.log('Start fetch');
+    
+    const fetchResponse = await fetch('https://www.svt.se/special/valjarbarometern/');
+    // TODO: Trash response, replace fetch with a headless client. (Playwright?)
+    const body = await fetchResponse.text();
+    console.log(body); */
+    
+
+    const { name } = request.query;
+    return response.status(200).send(`Hello ${name}!!!`);
 };
